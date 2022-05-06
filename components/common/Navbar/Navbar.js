ac3068ae1c8825cffc3a2ec4e32556c8f96ed177
JoachimBrasier <jo-brasier@outlook.com>
2022-05-06 18:31:11 +0200

    Navbar design

--- components/common/Navbar/Navbar.js
+++ components/common/Navbar/Navbar.js
@@ -1,3 +1,23 @@
-export default function Navbar() {
-  return <nav>Navbar</nav>;
-}
+import Link from 'next/link';
+
+import s from './Navbar.module.css';
+
+const Navbar = () => (
+  <nav className={s.root}>
+    <div className={s.container}>
+      <Link href="/">
+        <a className={s.logo}>Home</a>
+      </Link>
+      <div className={s.links}>
+        <Link href="/about">
+          <a className={s.link}>About</a>
+        </Link>
+        <Link href="/">
+          <a className={s.link}>FAQ</a>
+        </Link>
+      </div>
+    </div>
+  </nav>
+);
+
+export default Navbar;
