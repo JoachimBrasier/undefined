7a62220384ff42d570c632af6182e84a572b8f4e
JoachimBrasier <jo-brasier@outlook.com>
2022-05-06 22:39:37 +0200

    Add support for i18n

--- components/common/Footer/Footer.js
+++ components/common/Footer/Footer.js
@@ -1,11 +1,21 @@
+import { useRouter } from 'next/router';
+
+import i18n from './i18n.json';
+
 import s from './Footer.module.css';
 
-const Footer = () => (
-  <footer className={s.root}>
-    <div className={s.container}>
-      <span className={s.text}>Made with ❤️ for developers</span>
-    </div>
-  </footer>
-);
+const { loveMessage } = i18n;
+
+const Footer = () => {
+  const { locale } = useRouter();
+
+  return (
+    <footer className={s.root}>
+      <div className={s.container}>
+        <span className={s.text}>{loveMessage[locale]}</span>
+      </div>
+    </footer>
+  );
+};
 
 export default Footer;
