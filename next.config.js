9c3071d0717ea62a8e5c9a3f7d564774ccda8de7
JoachimBrasier <jo-brasier@outlook.com>
2022-05-06 17:55:53 +0200

    next config

--- next.config.js
+++ next.config.js
@@ -1,5 +1,10 @@
 module.exports = {
   trailingSlash: false,
+  i18n: {
+    locales: ['en', 'fr'],
+    defaultLocale: 'en',
+    localeDetection: false,
+  },
   images: {
     domains: ['res.cloudinary.com'],
   },
