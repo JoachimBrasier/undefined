1689303bc80faf28bf5cc288ed562b6f1453ed0f
JoachimBrasier <jo-brasier@outlook.com>
2022-05-06 22:39:32 +0200

    Add support for i18n

--- components/common/Navbar/Navbar.js
+++ components/common/Navbar/Navbar.js
@@ -1,23 +1,36 @@
 import Link from 'next/link';
+import { useRouter } from 'next/router';
+
+import i18n from './i18n.json';
+import NavbarLocaleSelect from './NavbarLocaleSelect';
 
 import s from './Navbar.module.css';
 
-const Navbar = () => (
-  <nav className={s.root}>
-    <div className={s.container}>
-      <Link href="/">
-        <a className={s.logo}>Home</a>
-      </Link>
-      <div className={s.links}>
-        <Link href="/about">
-          <a className={s.link}>About</a>
-        </Link>
+const { logo, links } = i18n;
+
+const Navbar = () => {
+  const { locale } = useRouter();
+
+  return (
+    <nav className={s.root}>
+      <div className={s.container}>
         <Link href="/">
-          <a className={s.link}>FAQ</a>
+          <a className={s.logo}>{logo[locale]}</a>
         </Link>
+        <div className={s.links}>
+          {links.map((link) => (
+            <Link key={link.href} href={link.href} as={link?.as}>
+              <a className={s.link}>{link.label[locale]}</a>
+            </Link>
+          ))}
+        </div>
+        <div className={s.divider} />
+        <div className={s.actions}>
+          <NavbarLocaleSelect />
+        </div>
       </div>
-    </div>
-  </nav>
-);
+    </nav>
+  );
+};
 
 export default Navbar;
---
+++ components/common/Navbar/NavbarLocaleSelect.js
@@ -0,0 +1,55 @@
+import Link from 'next/link';
+import { useRouter } from 'next/router';
+
+import { Listbox } from '@headlessui/react';
+import { CheckIcon, ChevronDownIcon } from '@heroicons/react/outline';
+import clsx from 'clsx';
+import { Fragment, useEffect, useState } from 'react';
+
+import i18n from './i18n.json';
+
+import s from './NavbarLocaleSelect.module.css';
+
+const { Button, Options, Option } = Listbox;
+const { locales } = i18n;
+
+const NavbarLocaleSelect = () => {
+  const router = useRouter();
+  const { locale: activeLocale } = router;
+  const [selectedLocale, setSelectedLocale] = useState(locales.find((locale) => locale.value === activeLocale));
+
+  useEffect(() => {
+    setSelectedLocale(locales.find((locale) => locale.value === activeLocale));
+  }, [activeLocale]);
+
+  return (
+    <Listbox value={selectedLocale}>
+      <div className={s.root}>
+        <Button className={s.button}>
+          {selectedLocale.label}
+          <ChevronDownIcon className={s.icon} />
+        </Button>
+        <Options className={s.dropdown}>
+          {locales.map((locale) => {
+            const { pathname, query, asPath } = router;
+
+            return (
+              <Option key={locale.value} value={locale} as={Fragment}>
+                {({ selected }) => (
+                  <Link href={{ pathname, query }} as={asPath} locale={locale.value} passHref>
+                    <li className={clsx(s.item, { [s.itemSelected]: selected })}>
+                      {locale.label}
+                      {selected && <CheckIcon className={s.icon} />}
+                    </li>
+                  </Link>
+                )}
+              </Option>
+            );
+          })}
+        </Options>
+      </div>
+    </Listbox>
+  );
+};
+
+export default NavbarLocaleSelect;
