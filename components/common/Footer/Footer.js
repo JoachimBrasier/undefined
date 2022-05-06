b0a3ea1f5c39112bb63354e895dfd7d87b0eb7c9
JoachimBrasier <jo-brasier@outlook.com>
2022-05-06 18:08:55 +0200

    footer design

--- components/common/Footer/Footer.js
+++ components/common/Footer/Footer.js
@@ -1,3 +1,11 @@
-export default function Footer() {
-  return <footer>Footer</footer>;
-}
+import s from './Footer.module.css';
+
+const Footer = () => (
+  <footer className={s.root}>
+    <div className={s.container}>
+      <span className={s.text}>Made with ❤️ for developers</span>
+    </div>
+  </footer>
+);
+
+export default Footer;
