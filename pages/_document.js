5e59efcaca753d631ebc7c926c10143108c23b40
JoachimBrasier <jo-brasier@outlook.com>
2022-05-07 15:37:31 +0200

    correct zoom on ios

---
+++ pages/_document.js
@@ -0,0 +1,15 @@
+import { Head, Html, Main, NextScript } from 'next/document';
+
+const Document = () => (
+  <Html>
+    <Head>
+      <meta name="viewport" content="width=device-width, user-scalable=no" />
+    </Head>
+    <body>
+      <Main />
+      <NextScript />
+    </body>
+  </Html>
+);
+
+export default Document;
