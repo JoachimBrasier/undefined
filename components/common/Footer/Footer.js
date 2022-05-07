b646a043cc25a01a72a8d1356d5073cd6d3d5350
JoachimBrasier <jo-brasier@outlook.com>
2022-05-07 10:40:15 +0200

    replace emoji with icon

--- components/common/Footer/Footer.js
+++ components/common/Footer/Footer.js
@@ -1,5 +1,8 @@
 import { useRouter } from 'next/router';
 
+import { HeartIcon } from '@heroicons/react/solid';
+import reactStringReplace from 'react-string-replace';
+
 import i18n from './i18n.json';
 
 import s from './Footer.module.css';
@@ -12,7 +15,15 @@ const Footer = () => {
   return (
     <footer className={s.root}>
       <div className={s.container}>
-        <span className={s.text}>{loveMessage[locale]}</span>
+        <span className={s.text}>
+          {reactStringReplace(loveMessage[locale], ' [icon-heart] ', () => (
+            <>
+              &nbsp;
+              <HeartIcon className={s.icon} />
+              &nbsp;
+            </>
+          ))}
+        </span>
       </div>
     </footer>
   );
