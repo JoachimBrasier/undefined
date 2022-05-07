dddb11fe837e38d1f09e1a36088e9e6d733b9d52
JoachimBrasier <jo-brasier@outlook.com>
2022-05-07 10:44:36 +0200

    add nprogress

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,17 @@
+import Router from 'next/router';
+
 import { ThemeProvider } from 'next-themes';
+import NProgress from 'nprogress';
 
 import { Layout } from 'components/common';
 
 import 'styles/global.css';
+
+NProgress.configure({ showSpinner: false });
+
+Router.onRouteChangeStart = () => NProgress.start();
+Router.onRouteChangeError = () => NProgress.done();
+Router.onRouteChangeComplete = () => NProgress.done();
 
 const App = ({ Component, pageProps }) => (
   <ThemeProvider attribute="class" enableSystem={false}>
