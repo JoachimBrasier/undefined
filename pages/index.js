df633a5dc9ab30ed626cab578f46b03e91478643
JoachimBrasier <jo-brasier@outlook.com>
2022-05-08 16:13:58 +0200

    remove unsued stuff

--- pages/index.js
+++ pages/index.js
@@ -1,20 +1,18 @@
 import { Grid, Hero, HomeProvider, List, Search, Sidebar } from 'components/pages/home';
 
-const Home = () => {
-  return (
-    <HomeProvider>
-      <div className="w-full max-w-screen-xl mx-auto px-4 py-6 flex gap-6">
-        <Sidebar />
-        <div className="flex-grow">
-          <Hero />
-          <Search />
-          <Grid />
-          <List />
-          <div />
-        </div>
+const Home = () => (
+  <HomeProvider>
+    <div className="w-full max-w-screen-xl mx-auto px-4 py-6 flex gap-6">
+      <Sidebar />
+      <div className="flex-grow">
+        <Hero />
+        <Search />
+        <Grid />
+        <List />
+        <div />
       </div>
-    </HomeProvider>
-  );
-};
+    </div>
+  </HomeProvider>
+);
 
 export default Home;
