df96124dc0f2337a4bc90e95c76c5c7aa50a8fa9
JoachimBrasier <jo-brasier@outlook.com>
2022-05-24 15:54:28 +0200

    add resource create route

--- pages/api/resources/index.js
+++ pages/api/resources/index.js
@@ -1,8 +1,11 @@
 import cookie from 'cookie';
+import Joi from 'joi';
 
+import withAuth from 'lib/middlewares/withAuth';
+import withJoi from 'lib/middlewares/withJoi';
 import prisma from 'lib/prisma';
 
-const handler = async (req, res) => {
+const handleGET = async (req, res) => {
   const cookies = cookie.parse(req.headers.cookie ?? '');
   const activeLocale = cookies.NEXT_LOCALE || 'en';
 
@@ -47,6 +50,63 @@ const handler = async (req, res) => {
   });
 
   return res.status(200).json(resources);
+};
+
+const postSchema = Joi.object({
+  title: Joi.string().min(2).max(50).required().trim(),
+  descriptions: Joi.object({
+    en: Joi.string().min(50).max(160).required().trim(),
+    fr: Joi.string().min(50).max(160).trim(),
+  }).required(),
+  url: Joi.string().uri(),
+  image: Joi.string().required(),
+  deprecated: Joi.boolean().default(false),
+  tags: Joi.array().items({ value: Joi.string().required(), label: Joi.string().required() }).max(5).required(),
+});
+
+const handlePOST = withAuth(
+  withJoi(async (req, res) => {
+    const session = req.session;
+    const { title, descriptions, image, url, tags, deprecated } = req.body;
+    const connectOrCreateTags = tags.map((tag) => ({
+      where: { slug: tag.value },
+      create: { slug: tag.value, names: { create: { en: tag.label } } },
+    }));
+
+    const result = await prisma.resource.create({
+      data: {
+        title,
+        deprecated: deprecated ?? false,
+        image,
+        url,
+        status: 'SUBMITTED',
+        descriptions: {
+          create: descriptions,
+        },
+        tags: {
+          connectOrCreate: connectOrCreateTags,
+        },
+        author: {
+          connect: {
+            id: session.user.id,
+          },
+        },
+      },
+    });
+
+    return res.status(201).json(result);
+  }, postSchema),
+);
+
+const handler = async (req, res) => {
+  switch (req.method) {
+    case 'GET':
+      return handleGET(req, res);
+    case 'POST':
+      return handlePOST(req, res);
+    default:
+      return res.status(405).json({});
+  }
 };
 
 export default handler;
